01738d92070b233c85982ebf8515847066afe7c2
Mário Rui <marinhocosta@live.com.pt>
2021-11-30 19:41:02 +0000

    finally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import allContacts from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(allContacts)
+
+  function handleAddClick() {
+    const randomIndex = Math.floor(Math.random() * contacts.length)
+    const newContact = contacts[randomIndex]
+
+    setContacts([newContact, ...contacts])
+  }
+
+  function handleSortPopularityClick() {
+    contacts.sort((a, b) => b.popularity - a.popularity)
+    setContacts([...contacts])
+  }
+
+  function handleSortNameClick() {
+    contacts.sort((a, b) => ('' + a.name).localeCompare(b.name))
+    setContacts([...contacts])
+  }
+
+  function handleDeleteClick(id) {
+    const filteredContacts = contacts.filter(contact => contact.id !== id)
+    setContacts(filteredContacts)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button 
+          onClick={handleAddClick}
+          style={{padding: '30px'}}
         >
-          Learn React
-        </a>
-      </header>
+        Add random contact
+        </button>
+
+        <button 
+          onClick={handleSortPopularityClick}
+          style={{padding: '30px'}}
+        >
+        Sort by Popularity
+        </button>
+
+        <button 
+          onClick={handleSortNameClick}
+          style={{padding: '30px'}}
+        >
+        Sort by Name
+        </button>
+
+
+        <table style={{width: '100%'}}>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              contacts.map((contact, index) => {
+                return(
+                  <tr key={contact.id + index}>
+                    <td><img src={contact.pictureUrl} alt="" height="100"/></td>
+                    <td><p>{contact.name}</p></td>
+                    <td><p>{contact.popularity}</p></td>
+                    <td><p>{contact.wonOscar ? '🏆' : ''}</p></td>
+                    <td><p>{contact.wonEmmy ? '🏆' : ''}</p></td>
+                    <td>
+                      <button 
+                        onClick={() => handleDeleteClick(contact.id)}
+                        style={{backgroundColor: 'blue', padding: '15px'}}
+                      >
+                       Delete 
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
     </div>
   );
 }
